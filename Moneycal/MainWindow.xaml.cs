c5055835a5049624789e0f9c44018e986c7ecfc4
SorawitKit <98862393+SorawitKit@users.noreply.github.com>
2022-02-03 09:43:04 +0700

    Start

---
+++ Moneycal/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Moneycal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double revenue = double.Parse(txtRevenue.Text);
+            double expense = double.Parse(txtExpense.Text);
+            double item    = double.Parse(txtItem.Text);
+            
+            double sum = revenue - expense;
+            double period = item / sum;
+            txtPeriod.Text = period.ToString();
+            
+
+
+
+        }
+    }
+}
